7e5436a92c2e834325fba1109a195c0672db9d7c
Lee <lxm016400@utdallas.edu>
2017-05-30 14:24:37 -0500

    Welcome To Git

---
+++ storm-example/src/main/java/com/learningstorm/storm_example/LearningStormSpout.java
@@ -0,0 +1,28 @@
+package com.learningstorm.storm_example;
+
+import java.util.Map;
+
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichSpout;
+
+public class LearningStormSpout extends BaseRichSpout {
+
+	public void nextTuple() {
+		
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void open(Map arg0, TopologyContext arg1, SpoutOutputCollector arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void declareOutputFields(OutputFieldsDeclarer arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
